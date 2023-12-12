35e624488b5c85a53bc4e9ceb18e1163bfec4f00
SO HUI <haejuuun925@gmail.com>
2023-12-12 21:49:23 +0900

    feat: create redux store

---
+++ src/store/index.js
@@ -0,0 +1,22 @@
+// 리덕스 로직 저장 index.js 파일
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if(action.type === 'increment') {
+        return {
+            counter: state.counter + 1,
+        };
+    }
+
+    if(action.type === 'decrement') {
+        return {
+            counter: state.counter - 1
+        };
+    }
+
+    return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
