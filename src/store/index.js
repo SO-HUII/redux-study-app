2e7efa80db28baf0ad0dcc2bf4e14eae2eeb4522
SO HUI <haejuuun925@gmail.com>
2023-12-21 18:21:55 +0900

    feat: toggle action type 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,34 @@
 // 리덕스 로직 저장 index.js 파일
 import { createStore } from 'redux';
 
-const counterReducer = (state = { counter: 0 }, action) => {
+
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment') {
         return {
             counter: state.counter + 1,
+            showCounter: state.showCounter
         };
     }
 
     if(action.type === 'increase') {
         return {
             counter: state.counter + action.amount,
+            showCounter: state.showCounter
         };
     }
 
     if(action.type === 'decrement') {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        };
+    }
+
+    if(action.type === 'toggle') {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         };
     }
 
