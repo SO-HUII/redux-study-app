cd83afc9293af097323303a0f91e0ed20d671aa4
SO HUI <haejuuun925@gmail.com>
2024-01-05 21:37:03 +0900

    feat: dispatch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import classes from './Header.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions } from '../store';
 
 const Header = () => {
+    const isAuth = useSelector(state => state.auth.isAuthenticated);
+    const dispatch = useDispatch();
+
+    const logoutHandler = (event) => {
+        event.preventDefault();
+
+        dispatch(authActions.login());
+    };
+
     return (
         <header className={classes.header}>
             <h1>Redux Auth</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href='/'>My Products</a>
-                    </li>
-                    <l1>
-                        <a href='/'>My Sales</a>
-                    </l1>
-                    <li>
-                        <button>Logout</button>
-                    </li>
-                </ul>
-            </nav>
+            {isAuth && (
+                <nav>
+                    <ul>
+                        <li>
+                            <a href='/'>My Products</a>
+                        </li>
+                        <l1>
+                            <a href='/'>My Sales</a>
+                        </l1>
+                        <li>
+                            <button onClick={logoutHandler}>Logout</button>
+                        </li>
+                    </ul>
+                </nav>
+            )}
         </header>
     );
 };
